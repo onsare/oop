da0b8a279003f4b74950c62340eaba22630e34d7
denis-onsare <denisonsare@gmail.com>
2017-04-05 01:40:56 +0300

    Added data abstraction and encapsulation example

--- oop.py
+++ oop.py
@@ -1,4 +1,4 @@
-# Demonstrate understanding of OOP concepts
+# Inheritance
 
 class Book(object):
 	def __init__(self, name , genre, author):
@@ -12,6 +12,25 @@ class The_NoteBook(Book):
        in the The_Notebook class
 	"""
 	def __init__(self, arg):
-		super(The_NoteBook, self).__init__()
 		self.arg = arg
 		
+#Data Abstraction and Encapsulation
+
+class Book(object):
+	"""By initializing the book class objects with
+       double underscore, we are simpy making the clas
+       inaccessible from outside the methos.
+       
+	"""
+	def __init__(self, name , genre, author):
+		self.__name = name
+		self.__genre = genre
+		self.__author = author
+
+class The_NoteBook(Book):
+	"""The_NoteBook class inherits all the Books property
+       such that, Everything that the Book class has, is contained 
+       in the The_Notebook class
+	"""
+	def __init__(self, arg):
+		self.arg = arg
