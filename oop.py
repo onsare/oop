74583a7808998a7a9d1ff3be6d4328e7863aed65
denis-onsare <denisonsare@gmail.com>
2017-04-07 05:39:54 +0300

    added more python definitions

--- oop.py
+++ oop.py
@@ -1,3 +1,37 @@
+
+#methods
+class Book(object):
+	"""Methods are basically functions within
+		a class. The first definition of a method
+		has to be a reference "self" to the instance
+		of the class
+	"""
+	def __init__(self, name):
+		self.name = name
+
+#constructor
+
+"""
+The __init__ keyword acts as a constructor in python
+Though strictly speaking python does not have its own
+explicit constructor method like other languages like C++
+"""
+def __init__(self, name):
+	self.name = name
+
+	
+#Destructor
+
+"""
+what holds true for python constructor is also true for destructors
+the keyword __del__ is used in place of a destructor
+"""	
+
+class Book(object):
+	def __init__(self,name):
+		self.name = name
+	def __del__(self):
+		print "Destructor initiated"
 # Inheritance
 
 class Book(object):
@@ -19,13 +53,18 @@ class The_NoteBook(Book):
 class Book(object):
 	"""By initializing the book class objects with
        double underscore, we are simpy making the clas
-       inaccessible from outside the methos.
+       inaccessible from outside the method.
+	   class Encapsulation(object):
+			def __init__(self, a, b, c):
+				self.public = a
+				self.__private = b
+				self.__protected = c
        
 	"""
 	def __init__(self, name , genre, author):
-		self.__name = name
+		self.name = name
 		self.__genre = genre
-		self.__author = author
+		self._author = author
 
 class The_NoteBook(Book):
 	"""The_NoteBook class inherits all the Books property
